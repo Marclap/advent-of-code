531242d97e267387d749cd1cdbfaf18c750ab27c
Marclap <jhona.cb991128@gmail.com>
2020-12-08 14:34:07 -0500

    Solution day 1

---
+++ 2020/Day1/problem.js
@@ -0,0 +1,36 @@
+var input = new Int8Array();
+
+function loadFile() {
+    var fs = require("fs");
+    var text = fs.readFileSync("input.txt").toString('utf-8');
+    input = text.split("\n").map(Number);
+}
+
+loadFile();
+
+const sumTwoNumbers = () => {
+    let complement = []
+    for(let number of input) {
+        complement[number] = true
+        toFind = 2020 - number
+        if(complement[toFind]){
+            console.log(number * toFind)
+            break
+        }
+    }
+}
+
+const sumThreeNumbers = () => {
+    for(let numberA of input) {
+        for(let numberB of input) {
+            for(let numberC of input) {
+                if((numberA + numberB + numberC) === 2020 && numberA != numberB && numberA != numberC && numberB != numberC) {
+                    console.log(numberA * numberB * numberC)
+                }
+            }
+        }
+    }
+}
+
+sumTwoNumbers()
+sumThreeNumbers()
